82381d279ea1d8304123183fe89a753eae6f1f67
Lee-matod <gustavo@losgo.cl>
2022-02-24 16:50:12 -0300

    Added a new 'flag' to flags.py file

--- flags/flags.py
+++ flags/flags.py
@@ -30,36 +30,58 @@ class DevFlags(commands.Cog):
             await ctx.send(f"Command `{command}` is not found.")
 
     @commands_.command(name="--source", aliases=["-src"], parent="dev", version=1)
-    async def root_flag_source(ctx: commands.Context, *, cmd: str = commands.Option(description="Command that should be fetched.")):
+    async def root_flag_source(ctx: commands.Context, flag: str = commands.Option(description="An optional flag that determines whether or not the source code should be just the command or the whole file."), *, cmd: str = commands.Option(name="command", description="Command that should be fetched.", default="")):
         """
         View the source code of a command. 
-        This flag is not exclusive to the `?dev` extension, however commands that aren't part of the cog should start with the specified character in `settings["source"]["not_dev_cmd"]`.
-        The bot's token is also hidden as `TOKEN`.
+        This flag is not exclusive to the `?dev` extension, however commands that aren't part of the cog should start with the specified character(s) in `settings["source"]["not_dev_cmd"]`.
+        The bot's token is hidden as `TOKEN`.
+        Add the `--file` flag before specifying the command to showcase the command's source code file instead of the plain command source code.
         """
+        if flag != "--file":
+            cmd = f"{flag} {cmd}".strip()
+            flag = False
+        else:
+            flag = True
         command = ctx.bot.get_command(f"dev {cmd}" if not cmd.startswith(settings["source"]["not_dev_cmd"]) else cmd)
         if not command:
             return await ctx.send(f"Command `{cmd}` is not found.")
-        lines, _ = inspect.getsourcelines(command.callback)
         directory = inspect.getsourcefile(command.callback)
         filename = settings["source"]["filename"] or pathlib.Path(inspect.getfile(command.callback)).name
-        if settings["source"]["use_file"]:
-            return await ctx.send(file=discord.File(filename), fp=io.BytesIO(''.join(lines).encode('utf-8')))
-        paginator = commands.Paginator(prefix="```py\n", suffix="```", max_size=1985, linesep='')
-        for line in lines:
-            if "`" in line:
-                line = line.replace("`", "\u200b`")
-            if ctx.bot.http.token in line:
-                line = line.replace(ctx.bot.http.token, "TOKEN")
-            paginator.add_line(line)
-        if settings["folder"]["root_folder"]:
-            directory = directory.replace(settings["folder"]["root_folder"], "/root/")
-        await ctx.send(f"{f'**{directory}**' if settings['source']['show_path'] else ''}\n{paginator.pages[0]}", view=Paginator(paginator, ctx.author.id, PATH=directory, show_path=settings["source"]["show_path"]))
+
+        if not flag:
+            lines, _ = inspect.getsourcelines(command.callback)
+            if settings["source"]["use_file"]:
+                return await ctx.send(file=discord.File(filename), fp=io.BytesIO(''.join(lines).encode('utf-8')))
+            paginator = commands.Paginator(prefix="```py\n", suffix="```", max_size=1985, linesep='')
+            for line in lines:
+                if "`" in line:
+                    line = line.replace("`", "\u200b`")
+                if ctx.bot.http.token in line:
+                    line = line.replace(ctx.bot.http.token, "TOKEN")
+                paginator.add_line(line)
+            if settings["folder"]["root_folder"]:
+                directory = directory.replace(settings["folder"]["root_folder"], "/root/")
+            return await ctx.send(f"{f'**{directory}**' if settings['source']['show_path'] else ''}\n{paginator.pages[0]}", view=Paginator(paginator, ctx.author.id, PATH=directory, show_path=settings["source"]["show_path"]))
+
+        with open(directory) as source:
+            if settings["source"]["use_file"]:
+                return await ctx.send(file=discord.File(directory))
+            paginator = commands.Paginator(prefix="```py\n", suffix="```", max_size=1985, linesep='')
+            for line in source.readlines():
+                if "`" in line:
+                    line = line.replace("`", "\u200b`")
+                if ctx.bot.http.token in line:
+                    line = line.replace(ctx.bot.http.token, "TOKEN")
+                paginator.add_line(line)
+            if settings["folder"]["root_folder"]:
+                directory = directory.replace(settings["folder"]["root_folder"], "/root/")
+            await ctx.send(f"{f'**{directory}**' if settings['source']['show_path'] else ''}\n{paginator.pages[0]}", view=Paginator(paginator, ctx.author.id, PATH=directory, show_path=settings["source"]["show_path"]))
 
     @commands_.command(name="--file", parent="dev", version=1)
     async def root_flag_file(ctx: commands.Context, *, directory: str):
         """
         View a file.
-        By default, the file is sent as a paginator, however this can be changed in settings["file"]["use_file"]. The bot's token is also hidden as `TOKEN`.
+        By default, the file is sent as a paginator, however this can be changed in settings["file"]["use_file"]. The bot's token is hidden as `TOKEN`.
         """
         raw_dir = directory
         if "/root/" in directory:
