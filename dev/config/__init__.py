4a5095c97f0dfc655702b3b25fa800fcfa9fb27b
Lee <89663192+Lee-matod@users.noreply.github.com>
2022-05-18 18:37:42 -0400

    created an `__init__` for `config`

---
+++ dev/config/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+dev.config
+~~~~~~~~~~
+
+Configuration-based commands used to mainly reconfigure and edit the bot.
+
+:copyright: Copyright 2022 Lee (Lee-matod)
+:license: Licensed under the Apache License, Version 2.0; see LICENSE for more details.
+"""
+
+
+from dev.config.bot import *
+from dev.config.over import *
+from dev.config.variables import *
+
+
+__all__ = (
+    "RootBot",
+    "RootOverride",
+    "RootVariables"
+)
