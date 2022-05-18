4a2f092e6f00d5b8a223191b21151deb27e3dce8
Lee <89663192+Lee-matod@users.noreply.github.com>
2022-05-18 18:45:42 -0400

    created an `__init__` for `flags`

---
+++ dev/flags/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+dev.flags
+~~~~~~~~~
+
+Flag-based commands to view specific attributes of commands.
+
+:copyright: Copyright 2022 Lee (Lee-matod)
+:license: Licensed under the Apache License, Version 2.0; see LICENSE for more details.
+"""
+
+
+from dev.flags.flags import *
+
+
+__all__ = (
+    "RootFlags"
+)
