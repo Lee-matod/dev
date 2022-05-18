b978e73ca1b626c9a1f877c91142091bca65f28c
Lee <89663192+Lee-matod@users.noreply.github.com>
2022-05-18 18:36:54 -0400

    fixed imports and some documentation issues

--- dev/handlers.py
+++ dev/handlers.py
@@ -9,22 +9,25 @@ Handlers that are used in the dev extension.
 :copyright: Copyright 2022 Lee (Lee-matod)
 :license: Licensed under the Apache License, Version 2.0; see LICENSE for more details.
 """
+
+
 from typing import *
 
-import re
 import asyncio
 import discord
+import re
 
 from discord.ext import commands
 from traceback import format_exception
 
-from dev.utils.utils import local_globals
 from dev.utils.startup import virtual_vars_format
+from dev.utils.utils import local_globals
+
 
 __all__ = (
-    "replace_vars",
     "ExceptionHandler",
-    "Paginator"
+    "Paginator",
+    "replace_vars"
 )
 
 
@@ -39,8 +42,8 @@ class ExceptionHandler:
     is_debug: :class:`bool`
         Whether the instance of this :class:`ExceptionHandler` is a debugger or not.
 
-    Attributes
-    ----------
+    Other Parameters
+    ----------------
     error: :class:`list[tuple[Exception, str]]`
         The errors that occurred during the lifetime of the process inside the context manager.
 
@@ -83,7 +86,7 @@ class ExceptionHandler:
             await self.message.add_reaction("💢")
         elif isinstance(exc_val, (TimeoutError, asyncio.TimeoutError)):
             await self.message.add_reaction("⏰")
-        elif isinstance(exc_val, (AssertionError, ImportError, ModuleNotFoundError, UnboundLocalError)):
+        elif isinstance(exc_val, (AssertionError, ImportError, UnboundLocalError)):
             await self.message.add_reaction("❓")
         elif isinstance(exc_val, (AttributeError, IndexError, KeyError, NameError, TypeError, UnicodeError, ValueError, commands.CommandInvokeError)):
             await self.message.add_reaction("❗")
@@ -109,16 +112,16 @@ class Paginator(discord.ui.View):
         instead of sending it as a message.
     """
 
-    def __init__(self, paginator: commands.Paginator, user_id: int, **kwargs):
+    def __init__(self, paginator: commands.Paginator, owner: int, **kwargs):
         super().__init__()
         self.paginator = paginator
         self.display_page = 0
-        self.user_id = user_id
+        self.owner = owner
         self.is_embed: discord.Embed = kwargs.pop("embed", False)
 
-    @discord.ui.button(emoji="⏪", style=discord.ButtonStyle.primary, disabled=True, custom_id="rewind")
-    async def first_page(self, interaction: discord.Interaction, button: discord.Button) -> Optional[discord.Message]:
-        if interaction.user.id != self.user_id:
+    @discord.ui.button(emoji="⏪", style=discord.ButtonStyle.primary, disabled=True)
+    async def first_page(self, interaction: discord.Interaction, _) -> Optional[discord.Message]:  # 'button' is not being used
+        if interaction.user.id != self.owner:
             return
         self.display_page = 0
         self.enable_or_disable(rewind_=True, previous_=True)
@@ -128,28 +131,24 @@ class Paginator(discord.ui.View):
             return await interaction.response.edit_message(embed=self.is_embed, view=self)
         await interaction.response.edit_message(content=f"{self.paginator.pages[self.display_page]}", view=self)
 
-    @discord.ui.button(emoji="◀", style=discord.ButtonStyle.success, disabled=True, custom_id="previous")
-    async def previous_page(self, interaction: discord.Interaction, button: discord.Button) -> Optional[discord.Message]:
-        if interaction.user.id != self.user_id:
+    @discord.ui.button(emoji="◀", style=discord.ButtonStyle.success, disabled=True)
+    async def previous_page(self, interaction: discord.Interaction, _) -> Optional[discord.Message]:  # 'button' is not being used
+        if interaction.user.id != self.owner:
             return
         self.display_page -= 1
         if self.display_page == 0:
-            self.update_display_page()
             self.enable_or_disable(rewind_=True, previous_=True)
-            if self.is_embed:
-                self.is_embed.description = self.paginator.pages[self.display_page]
-                return await interaction.response.edit_message(embed=self.is_embed, view=self)
-            return await interaction.response.edit_message(content=f"{self.paginator.pages[self.display_page]}", view=self)
-        self.enable_or_disable(next_=False, fastforward_=False)
+        else:
+            self.enable_or_disable(next_=False, fastforward_=False)
         self.update_display_page()
         if self.is_embed:
             self.is_embed.description = self.paginator.pages[self.display_page]
             return await interaction.response.edit_message(embed=self.is_embed, view=self)
         await interaction.response.edit_message(content=f"{self.paginator.pages[self.display_page]}", view=self)
 
-    @discord.ui.button(label="1", style=discord.ButtonStyle.red, custom_id="current_stop")
-    async def current_stop(self, interaction: discord.Interaction, button: discord.Button) -> None:
-        if interaction.user.id != self.user_id:
+    @discord.ui.button(label="1", style=discord.ButtonStyle.red)
+    async def current_stop(self, interaction: discord.Interaction, _) -> None:  # 'button' is not being used
+        if interaction.user.id != self.owner:
             return
         button: discord.Button
         for button in self.children:
@@ -157,9 +156,9 @@ class Paginator(discord.ui.View):
                 button.disabled = True
         await interaction.response.edit_message(view=self)
 
-    @discord.ui.button(emoji="▶", style=discord.ButtonStyle.success, custom_id="next")
-    async def next_page(self, interaction: discord.Interaction, button: discord.Button) -> Optional[discord.Message]:
-        if interaction.user.id != self.user_id:
+    @discord.ui.button(emoji="▶", style=discord.ButtonStyle.success)
+    async def next_page(self, interaction: discord.Interaction, _) -> Optional[discord.Message]:  # 'button' is not being used
+        if interaction.user.id != self.owner:
             return
         self.display_page += 1
         try:
@@ -181,9 +180,9 @@ class Paginator(discord.ui.View):
             self.enable_or_disable(rewind_=True, previous_=True, next_=True, fastforward_=True)
             await interaction.response.edit_message(view=self)
 
-    @discord.ui.button(emoji="⏩", style=discord.ButtonStyle.primary, custom_id="fastforward")
-    async def last_page(self, interaction: discord.Interaction, button: discord.Button) -> Optional[discord.Message]:
-        if interaction.user.id != self.user_id:
+    @discord.ui.button(emoji="⏩", style=discord.ButtonStyle.primary)
+    async def last_page(self, interaction: discord.Interaction, _) -> Optional[discord.Message]:  # 'button' is not being used
+        if interaction.user.id != self.owner:
             return
         self.display_page = len(self.paginator.pages) - 1
         try:
@@ -199,24 +198,17 @@ class Paginator(discord.ui.View):
             await interaction.response.edit_message(view=self)
 
     @discord.ui.button(label="Delete", style=discord.ButtonStyle.danger, custom_id="delete", emoji="🗑️")
-    async def delete_page(self, interaction: discord.Interaction, button: discord.Button) -> None:
+    async def delete_page(self, interaction: discord.Interaction, _) -> None:  # 'button' is not being used
         await interaction.message.delete()
 
     def enable_or_disable(self, *, rewind_=False, previous_=False, next_=False, fastforward_=False) -> None:
-        button: discord.Button
-        b_rewind = [button for button in self.children if button.custom_id == "rewind"][0]
-        b_previous = [button for button in self.children if button.custom_id == "previous"][0]
-        b_next = [button for button in self.children if button.custom_id == "next"][0]
-        b_fastforward = [button for button in self.children if button.custom_id == "fastforward"][0]
-        b_rewind.disabled = rewind_
-        b_previous.disabled = previous_
-        b_next.disabled = next_
-        b_fastforward.disabled = fastforward_
+        self.last_page.disabled = rewind_
+        self.previous_page.disabled = previous_
+        self.next_page.disabled = next_
+        self.first_page.disabled = fastforward_
 
     def update_display_page(self) -> None:
-        b: discord.Button
-        current_stop = [b for b in self.children if b.custom_id == "current_stop"][0]
-        current_stop.label = self.display_page + 1
+        self.current_stop.label = self.display_page + 1
 
 
 def replace_vars(string: str) -> str:
