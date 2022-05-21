a35663c4273a5b89885340e0523bcf2dfe12a5a3
Lee <89663192+Lee-matod@users.noreply.github.com>
2022-05-21 15:56:40 -0400

    removed unused import statements - still have to rework this

--- dev/experimental/python.py
+++ dev/experimental/python.py
@@ -9,21 +9,11 @@ Direct evaluation or execution of Python code.
 :copyright: Copyright 2022 Lee (Lee-matod)
 :license: Licensed under the Apache License, Version 2.0; see LICENSE for more details.
 """
-import io
-import ast
-import inspect
-import contextlib
 
-import discord
+
 from discord.ext import commands
-from typing import Dict, Any, AsyncGenerator, Optional
 
-from dev.converters import __previous__
-from dev.handlers import ExceptionHandler, replace_vars
-
-from dev.utils.startup import Settings
-from dev.utils.functs import clean_code, send
-from dev.utils.baseclass import root, Root, GlobalLocals
+from dev.utils.baseclass import root, Root
 
 # TODO: I have to reword this
 # cause omg was it annoying to eval stuff
@@ -31,5 +21,4 @@ from dev.utils.baseclass import root, Root, GlobalLocals
 class RootPython(Root):
     def __init__(self, bot: commands.Bot):
         super().__init__(bot)
-
-
+        
