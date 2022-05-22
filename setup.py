be61f3ec706550f1754d23316c632e7ed69c94d5
Lee <89663192+Lee-matod@users.noreply.github.com>
2022-05-21 23:12:20 -0400

    added an initial `setup.py`

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r") as file:
+    readme = file.read()
+
+packages = [
+    "dev",
+    "config",
+    "experimental",
+    "flags"
+]
+
+setup(name="dev",
+      author="Lee-matod",
+      url="https://github.com/Lee-matod/dev",
+      version="1.0.0",
+      packages=packages,
+      license="Apache 2.0",
+      description="A debugging, testing and editing cog for discord.py",
+      long_description=readme,
+      long_description_content_type="text/markdown",
+      include_package_data=True,
+      python_requires=">=3.9.0",
+)
