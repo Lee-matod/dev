2ca49a510695311b868f74aabb5e0c5715823148
Lee <89663192+Lee-matod@users.noreply.github.com>
2022-05-21 23:18:12 -0400

    cleaned it up a bit

--- setup.py
+++ setup.py
@@ -7,15 +7,16 @@ packages = [
     "dev"
 ]
 
-setup(name="dev",
-      author="Lee-matod",
-      url="https://github.com/Lee-matod/dev",
-      version="1.0.0",
-      packages=packages,
-      license="Apache 2.0",
-      description="A debugging, testing and editing cog for discord.py",
-      long_description=readme,
-      long_description_content_type="text/markdown",
-      include_package_data=True,
-      python_requires=">=3.9.0",
+setup(
+    name="dev",
+    author="Lee-matod",
+    url="https://github.com/Lee-matod/dev",
+    version="1.0.0",
+    packages=packages,
+    license="Apache 2.0",
+    description="A debugging, testing and editing cog for discord.py",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    python_requires=">=3.9.0",
 )
