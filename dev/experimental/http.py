cf963b9810dfd45bbf0168250767ce5a0230b6ea
Lee <89663192+Lee-matod@users.noreply.github.com>
2022-05-27 11:12:05 -0400

    added new format for json, and new implementation for status mode

--- dev/experimental/http.py
+++ dev/experimental/http.py
@@ -17,10 +17,12 @@ import io
 import json
 
 from discord.ext import commands
-from typing import Dict, Literal
+from typing import Literal
 
 from dev.converters import LiteralModes
 from dev.handlers import replace_vars
+
+from dev.misc.http_responses import responses
 
 from dev.utils.baseclass import Root, root
 from dev.utils.functs import flag_parser, send
@@ -31,27 +33,31 @@ class RootHTTP(Root):
     def __init__(self, bot: commands.Bot):
         super().__init__(bot)
 
-    @root.command(name="http", parent="dev", virtual_vars=True)
-    async def root_http(self, ctx: commands.Context, url: str, mode: LiteralModes[Literal["status", "json", "text", "read"]], allow_redirects: bool = False, *, options: str = None):
-        """Get a response from a specified url. Response modes can differ.
+    @root.group(name="http", parent="dev", virtual_vars=True)
+    async def root_http_get(self, ctx: commands.Context, url: str, mode: LiteralModes[Literal["status", "json", "text", "read"]], allow_redirects: bool = False, *, options: str = None):
+        """View the contents of an url. Response modes can differ.
         **Modes:**
-        `status` = Return the status code of the website.
+        `content` = Get the contents of the url.
         `json` = Convert the response to JSON. This isn't always available.
-        `text` = Send the response as text.
         `read` = Read the response and return it.
+        `status` = Return the status code of the website.
+        `text` = Send the response as text.
         """
         if mode is None:
             return
         kwargs = flag_parser(replace_vars(options or ''), Settings.FLAG_DELIMITER.strip())
-        async with aiohttp.ClientSession() as SESSION:
+        async with aiohttp.ClientSession() as session:
             try:
-                async with SESSION.get(replace_vars(url), allow_redirects=allow_redirects, **kwargs) as request:
+                async with session.get(replace_vars(url), allow_redirects=allow_redirects, **kwargs) as request:
                     if mode == "status":
-                        await send(ctx, f"Response **{request.status}**")
+                        await send(ctx, discord.Embed(title=f"Status {(status := str(request.status))}", description=f"{(f'{t}.' if (t := responses.get(status[0])) else '')}{' ‒ ' if (desc := responses.get(status)) else ''}{f'{desc}.' if desc else ''}".strip(), color=discord.Color.blurple(), url=f"https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/{status}"))
                     elif mode == "json":
                         try:
-                            file = await request.json()
-                            await send(ctx, discord.File(filename="response.json", fp=io.BytesIO(file.encode("utf-8"))))
+                            json_dict = await request.json()
+                            with io.BytesIO() as binary_file:
+                                binary_file.write(json.dumps(json_dict, indent=2).encode("utf-8"))
+                                binary_file.seek(0)
+                                await send(ctx, discord.File(filename="response.json", fp=binary_file))
                         except json.JSONDecodeError:
                             await send(ctx, "Unable to decode to JSON.")
                     elif mode == "text":
@@ -65,3 +71,5 @@ class RootHTTP(Root):
                             await send(ctx, discord.File(filename="response", fp=binary_file))
             except aiohttp.InvalidURL:
                 await send(ctx, "Invalid URL link.")
+            except aiohttp.ClientConnectorError:
+                await send(ctx, "Cannot connect to host. Name or service not known.")
