9880efa4a8360fba4ecbffc44c0af28eeef6176d
Lee <89663192+Lee-matod@users.noreply.github.com>
2022-06-03 18:37:58 -0400

    implemented new parent cogs
    * Updated __all__
    * Added __title__, __author__ and __license__

--- dev/__init__.py
+++ dev/__init__.py
@@ -4,12 +4,15 @@
 dev
 ~~~
 
-A simple debugging, editing and testing extension for discord.py version 2.0 (master).
+A simple debugging, editing and testing extension for discord.py version 2.0.
 
 :copyright: Copyright 2022 Lee (Lee-matod)
 :license: Licensed under the Apache License, Version 2.0; see LICENSE for more details.
 """
 
+__title__ = "dev"
+__author__ = "Lee"
+__license__ = "Apache 2.0"
 __version__ = "1.0.0a"
 
 
@@ -17,8 +20,8 @@ from dev.__main__ import *
 from dev.converters import *
 from dev.handlers import *
 
-from dev.config import RootBot, RootManagement, RootOver, RootVariables
-from dev.experimental import RootHTTP, RootInvoke, RootPython
+from dev.config import RootConfig
+from dev.experimental import RootExperimental
 from dev.flags import RootFlags
 
 from dev.utils.baseclass import *
@@ -28,25 +31,28 @@ from dev.utils.utils import *
 
 
 __all__ = (
+    "BoolInput",
     "CodeblockConverter",
     "Command",
     "Dev",
+    "ExceptionHandler",
     "GlobalLocals",
     "Group",
+    "LiteralModes",
+    "Paginator",
     "Root",
     "Settings",
+    "convert_str_to_bool",
+    "convert_str_to_ints",
+    "flag_parser",
     "replace_vars",
     "root",
     "send"
 )
 
-cogs = [RootBot, RootCommand, RootFlags, RootHTTP, RootInvoke, RootManagement, RootOver, RootPython, RootVariables]
 
-
-class Dev(*cogs):
-    """
-    The frontend root cog of the dev extension.
-    """
+class Dev(RootCommand, RootConfig, RootExperimental, RootFlags):
+    """The frontend root cog of the dev extension."""
 
 
 async def setup(bot: commands.Bot):
