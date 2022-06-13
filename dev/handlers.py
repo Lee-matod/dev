883e7fa3c9ee6a905b478623ab99027d68d53944
Lee <89663192+Lee-matod@users.noreply.github.com>
2022-06-13 18:35:25 -0400

    updated types and documentation

--- dev/handlers.py
+++ dev/handlers.py
@@ -49,24 +49,29 @@ __all__ = (
 
 
 class BoolInput(discord.ui.View):
-    """Request the user a yes or no answer.
+    """Allows the user to submit a yes or no answer through buttons.
+    If the user clicks on yes, a function is called and the view is removed.
+
+    Examples
+    --------
+    .. codeblock:: python3
+        async def check(ctx: commands.Context):
+            await ctx.send("We shall continue!")
+        await ctx.send("Would you like to continue?", view=BoolInput(ctx.author, check, ctx))
 
     Parameters
     ----------
-    author: Union[:class:`discord.Member`, :class:`int`]
-        The author of the message, a.k.a. ctx.author
-
+    author: Union[:class:`discord.abc.User`, :class:`int`]
+        The author of the message. It can be either their ID or User object.
     func: :class:`callable`[Any, Any]
-        The function that should be executed if the user clicks on the yes button.
-
+        The function that should get called if the user clicks on the yes button.
     args: :class:`Any`
-        The arguments that should be passed into the function, if any.
-
+        The arguments that should be passed into the function once it gets executed, if any.
     kwargs: :class:`Any`
-        The key-word arguments that should be passed into the function, if any.
-
+        The keyword arguments that should be passed into the function once it gets executed, if any.
     """
-    def __init__(self, author: Union[discord.Member, int], func: Callable[P, Any], *args: Any, **kwargs: Any):
+
+    def __init__(self, author: Union[discord.abc.User, int], func: Callable[P, Any], *args: Any, **kwargs: Any):
         super().__init__()
         self.func = func
         self.author: int = author.id if isinstance(author, discord.Member) else author
@@ -92,16 +97,23 @@ class BoolInput(discord.ui.View):
 
 class ExceptionHandler:
     """Handle any exceptions in an async context manager.
+    If any exceptions are raised during the process' lifetime, the bot will try to add
+    reactions depending on the exception value.
+
+    💢 – Syntax errors (EOFError, IndentationError).
+    ⏰ – Timeout errors (asyncio.TimeoutError, TimeoutError).
+    ❓ – Reference errors (ImportError, NameError).
+    ❗ – Runtime errors (IndexError, KeyError, TypeError, ValueError).
+    ⁉ – Arithmatic errors (ZeroDivisionError, FloatingPointError).
+    ‼ – Any other errors that don't fall under any of the previous categories.
 
     Parameters
     ----------
     message: :class:`discord.Message`
-        The message that the reactions will be added to
-
-    Returns
-    -------
-    ExceptionHandler
-        When entering the context manager, the instance of it is returned.
+        The message that the reactions will be added to.
+    send_traceback: :class:`bool`
+        Whether to send a traceback if an exception is raised.
+        Defaults to ``False``.
     """
     error: List[Tuple[str, str]] = []
     debug: bool = False
@@ -126,9 +138,9 @@ class ExceptionHandler:
                 await self.message.add_reaction("💢")
             elif isinstance(exc_val, (TimeoutError, asyncio.TimeoutError)):
                 await self.message.add_reaction("⏰")
-            elif isinstance(exc_val, (AssertionError, ImportError, UnboundLocalError)):
+            elif isinstance(exc_val, (AssertionError, ImportError, NameError, UnboundLocalError)):
                 await self.message.add_reaction("❓")
-            elif isinstance(exc_val, (AttributeError, IndexError, KeyError, NameError, TypeError, UnicodeError, ValueError, commands.CommandInvokeError)):
+            elif isinstance(exc_val, (AttributeError, IndexError, KeyError, TypeError, UnicodeError, ValueError, commands.CommandInvokeError)):
                 if isinstance(exc_val, commands.CommandInvokeError):
                     exc_val = exc_val.original
                 await self.message.add_reaction("❗")
@@ -143,41 +155,54 @@ class ExceptionHandler:
 
     @classmethod
     def cleanup(cls):
+        """Deletes any tracebacks that were saved if send_traceback was set to True.
+        This method should always get called once you have finished handling any tracebacks
+        """
         cls.error = []
         cls.debug = False
 
 
 class Paginator(discord.ui.View):
-    """Creates a paginator interface.
+    """A paginator interface that allows you to iterate through pages
+    if a message exceeds character limits using buttons.
+
+    Examples
+    --------
+    .. codeblock:: python3
+
+        paginator = commands.Paginator(...)
+        for line in some_long_text.split("\n"):
+            paginator.add_line(line)
+        interface = dev.Paginator(paginator, ctx.author.id)
+        await ctx.send(interface.pages[0], view=interface)
 
     Parameters
     ----------
     paginator: :class:`commands.Paginator`
-        Paginator that this interface will use.
-    user_id: :class:`int`
-        The command author's ID.
-    is_embed: Optional[:class:`discord.Embed`]
-        If an embed is passed to this argument, the interface will use its description to change pages
-        instead of sending it as a message.
+        The paginator class from where to get the pages from.
+    owner: :class:`int`
+        The ID of the author of the command's invoked message.
+    embed: Optional[:class:`discord.Embed`]
+        If the message is an embed, then the embed should be passed here.
     """
 
-    def __init__(self, paginator: commands.Paginator, owner: int, **kwargs):
+    def __init__(self, paginator: commands.Paginator, owner: int, *, embed: discord.Embed = None):
         super().__init__()
         self.paginator = paginator
         self.display_page = 0
         self.owner = owner
-        self.is_embed: discord.Embed = kwargs.pop("embed", False)
+        self.embed = embed  # type: ignore
 
     @discord.ui.button(emoji="⏪", style=discord.ButtonStyle.primary, disabled=True)
     async def first_page(self, interaction: discord.Interaction, _) -> Optional[discord.Message]:  # 'button' is not being used
         if interaction.user.id != self.owner:
             return
         self.display_page = 0
-        self.enable_or_disable(rewind_=True, previous_=True)
+        self.enable_or_disable(rewind=True, previous=True)
         self.update_display_page()
-        if self.is_embed:
-            self.is_embed.description = self.paginator.pages[self.display_page]
-            return await interaction.response.edit_message(embed=self.is_embed, view=self)
+        if self.embed:
+            self.embed.description = self.paginator.pages[self.display_page]
+            return await interaction.response.edit_message(embed=self.embed, view=self)
         await interaction.response.edit_message(content=f"{self.paginator.pages[self.display_page]}", view=self)
 
     @discord.ui.button(emoji="◀", style=discord.ButtonStyle.success, disabled=True)
@@ -186,13 +211,13 @@ class Paginator(discord.ui.View):
             return
         self.display_page -= 1
         if self.display_page == 0:
-            self.enable_or_disable(rewind_=True, previous_=True)
+            self.enable_or_disable(rewind=True, previous=True)
         else:
-            self.enable_or_disable(next_=False, fastforward_=False)
+            self.enable_or_disable(next_=False, fastforward=False)
         self.update_display_page()
-        if self.is_embed:
-            self.is_embed.description = self.paginator.pages[self.display_page]
-            return await interaction.response.edit_message(embed=self.is_embed, view=self)
+        if self.embed:
+            self.embed.description = self.paginator.pages[self.display_page]
+            return await interaction.response.edit_message(embed=self.embed, view=self)
         await interaction.response.edit_message(content=f"{self.paginator.pages[self.display_page]}", view=self)
 
     @discord.ui.button(label="1", style=discord.ButtonStyle.red)
@@ -214,19 +239,19 @@ class Paginator(discord.ui.View):
             pages = self.paginator.pages[self.display_page]
             if self.display_page == len(self.paginator.pages) - 1:
                 self.update_display_page()
-                self.enable_or_disable(next_=True, fastforward_=True)
-                if self.is_embed:
-                    self.is_embed.description = self.paginator.pages[self.display_page]
-                    return await interaction.response.edit_message(embed=self.is_embed, view=self)
+                self.enable_or_disable(next_=True, fastforward=True)
+                if self.embed:
+                    self.embed.description = self.paginator.pages[self.display_page]
+                    return await interaction.response.edit_message(embed=self.embed, view=self)
                 return await interaction.response.edit_message(content=f"{pages}", view=self)
-            self.enable_or_disable(rewind_=False, previous_=False)
+            self.enable_or_disable(rewind=False, previous=False)
             self.update_display_page()
-            if self.is_embed:
-                self.is_embed.description = self.paginator.pages[self.display_page]
-                return await interaction.response.edit_message(embed=self.is_embed, view=self)
+            if self.embed:
+                self.embed.description = self.paginator.pages[self.display_page]
+                return await interaction.response.edit_message(embed=self.embed, view=self)
             await interaction.response.edit_message(content=f"{pages}", view=self)
         except IndexError:
-            self.enable_or_disable(rewind_=True, previous_=True, next_=True, fastforward_=True)
+            self.enable_or_disable(rewind=True, previous=True, next_=True, fastforward=True)
             await interaction.response.edit_message(view=self)
 
     @discord.ui.button(emoji="⏩", style=discord.ButtonStyle.primary)
@@ -236,32 +261,34 @@ class Paginator(discord.ui.View):
         self.display_page = len(self.paginator.pages) - 1
         try:
             pages = self.paginator.pages[self.display_page]
-            self.enable_or_disable(next_=True, fastforward_=True)
+            self.enable_or_disable(next_=True, fastforward=True)
             self.update_display_page()
-            if self.is_embed:
-                self.is_embed.description = self.paginator.pages[self.display_page]
-                return await interaction.response.edit_message(embed=self.is_embed, view=self)
+            if self.embed:
+                self.embed.description = self.paginator.pages[self.display_page]
+                return await interaction.response.edit_message(embed=self.embed, view=self)
             await interaction.response.edit_message(content=f"{pages}", view=self)
         except IndexError:
-            self.enable_or_disable(rewind_=True, previous_=True, next_=True, fastforward_=True)
+            self.enable_or_disable(rewind=True, previous=True, next_=True, fastforward=True)
             await interaction.response.edit_message(view=self)
 
     @discord.ui.button(label="Delete", style=discord.ButtonStyle.danger, custom_id="delete", emoji="🗑️")
     async def delete_page(self, interaction: discord.Interaction, _) -> None:  # 'button' is not being used
         await interaction.message.delete()
 
-    def enable_or_disable(self, *, rewind_=False, previous_=False, next_=False, fastforward_=False) -> None:
-        self.last_page.disabled = rewind_
-        self.previous_page.disabled = previous_
+    def enable_or_disable(self, *, rewind=False, previous=False, next_=False, fastforward=False) -> None:
+        self.last_page.disabled = rewind
+        self.previous_page.disabled = previous
         self.next_page.disabled = next_
-        self.first_page.disabled = fastforward_
+        self.first_page.disabled = fastforward
 
     def update_display_page(self) -> None:
         self.current_stop.label = self.display_page + 1
 
 
 def replace_vars(string: str) -> str:
-    """Replace any instance of a virtual variable with its value and return it.
+    """Replaces any instance of a virtual variables with their respective values and return it the parsed string.
+
+    Instances of the variables will not get converted if a value is not found.
 
     Parameters
     ----------
