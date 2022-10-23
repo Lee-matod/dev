b639b5f96b27710c73842fc9bc8a44ac9dec4e96
Lee-matod <lee.underscore.matod@gmail.com>
2022-10-22 22:58:32 -0300

    Add future imports

--- dev/__main__.py
+++ dev/__main__.py
@@ -9,6 +9,8 @@ Root command and other that do not fall under any other category.
 :copyright: Copyright 2022 Lee (Lee-matod)
 :license: Licensed under the Apache License, Version 2.0; see LICENSE for more details.
 """
+from __future__ import annotations
+
 import os
 import sys
 import time
