41cb82bb39116c75ebf3e8b364abd4ace0536727
Lee-matod <lee.underscore.matod@gmail.com>
2022-12-29 16:07:53 -0300

    Add annotations import

--- dev/utils/utils.py
+++ dev/utils/utils.py
@@ -9,6 +9,8 @@ Basic utilities used within the dev extension.
 :copyright: Copyright 2022 Lee (Lee-matod)
 :license: Licensed under the Apache License, Version 2.0; see LICENSE for more details.
 """
+from __future__ import annotations
+
 from typing import TYPE_CHECKING
 from discord.utils import escape_markdown, escape_mentions
 
