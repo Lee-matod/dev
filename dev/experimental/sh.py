0d686a6e5618ecda1b4dc747a5efd296de41f641
Lee-matod <lee.underscore.matod@gmail.com>
2023-01-11 23:21:30 -0300

    Move active_shell_sessions.remove to a finally block

--- dev/experimental/sh.py
+++ dev/experimental/sh.py
@@ -38,34 +38,35 @@ class RootShell(Root):
             return await send(ctx, "A shell session is already active, please close it before starting a new one.")
         self.active_shell_sessions.append(ctx.author.id)
         shell = ShellSession()
-        with shell(script) as process:  # type: ignore
-            await process.run_until_complete(ctx)
-
-        def check(msg: discord.Message) -> bool:
-            return (
-                    msg.author == ctx.author
-                    and msg.channel == ctx.channel
-                    and msg.content.startswith(shell.interface.lower())
-            )
-
-        while not shell.terminated and not process.is_alive:
-            try:
-                message: discord.Message = await self.bot.wait_for("message", check=check, timeout=30)
-            except asyncio.TimeoutError:
-                self.active_shell_sessions.remove(ctx.author.id)
-                return await send(
-                    ctx,
-                    shell.set_exit_message(f"Return code: `{process.close_code}`"),
-                    forced_pagination=False,
-                    paginator=shell.paginator
-                )
-            with shell(message.content[len(shell.interface):].strip()) as process:
+        try:
+            with shell(script) as process:  # type: ignore
                 await process.run_until_complete(ctx)
-        await send(
-            ctx,
-            shell.set_exit_message(f"Return code: `{process.close_code}`"),
-            forced_pagination=False,
-            paginator=shell.paginator
-        )
-        self.active_shell_sessions.remove(ctx.author.id)
+
+            def check(msg: discord.Message) -> bool:
+                return (
+                        msg.author == ctx.author
+                        and msg.channel == ctx.channel
+                        and msg.content.startswith(shell.interface.lower())
+                )
+
+            while not shell.terminated and not process.is_alive:
+                try:
+                    message: discord.Message = await self.bot.wait_for("message", check=check, timeout=30)
+                except asyncio.TimeoutError:
+                    return await send(
+                        ctx,
+                        shell.set_exit_message(f"Return code: `{process.close_code}`"),
+                        forced_pagination=False,
+                        paginator=shell.paginator
+                    )
+                with shell(message.content[len(shell.interface):].strip()) as process:
+                    await process.run_until_complete(ctx)
+            await send(
+                ctx,
+                shell.set_exit_message(f"Return code: `{process.close_code}`"),
+                forced_pagination=False,
+                paginator=shell.paginator
+            )
+        finally:
+            self.active_shell_sessions.remove(ctx.author.id)
 
