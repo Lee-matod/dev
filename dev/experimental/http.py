33cec9b47b9de4e1293a7aa00405b868a389e548
Lee-matod <lee.underscore.matod@gmail.com>
2023-01-13 18:50:45 -0300

    Rework response handling

--- dev/experimental/http.py
+++ dev/experimental/http.py
@@ -31,6 +31,26 @@ if TYPE_CHECKING:
 
     from dev import types
 
+CONTENT_TYPES: dict[str, str] = {
+    "application/zip": "zip",
+    "application/xml": "xml",
+    "application/json": "json",
+    "application/ogg": "ogg",
+    "application/pdf": "pdf",
+    "application/xhtml+xml": "xhtml",
+    "application/javascript": "js",
+    "image/gif": "gif",
+    "image/jpeg": "jpeg",
+    "image/png": "png",
+    "text/css": "css",
+    "text/csv": "csv",
+    "text/html": "html",
+    "text/javascript": "js",
+    "text/xml": "xml",
+    "video/mp4": "mp4",
+    "video/webm": "webm"
+}
+
 
 class RootHTTP(Root):
 
@@ -39,61 +59,69 @@ class RootHTTP(Root):
             self,
             ctx: commands.Context[types.Bot],
             url: str,
-            mode: LiteralModes[Literal["json", "read", "status", "text"]],
+            mode: LiteralModes[Literal["json", "read", "status"]],
             allow_redirects: bool = False,
             *,
             options: str | None = None
     ):
-        """Send an HTTP request using GET method.
+        """Send an HTTP GET request to *url*.
+        *options* should be written in valid JSON format.
         **Modes:**
         `json` = Converts the response to JSON. This isn't always available.
         `read` = Read the response and return it.
         `status` = Return the status code of the website.
-        `text` = Send the response as text.
         """
         if mode is None:
             return
+        #  Perhaps '>' is a needed literal in a parameter, so we shouldn't remove it
+        #  if not necessary
+        if url.startswith("<") and url.endswith(">"):
+            url = url[1:-1]
         try:
             kwargs = flag_parser(replace_vars(options or '', Root.scope), Settings.flag_delimiter.strip())
         except json.JSONDecodeError as exc:
             return await send(ctx, f"{exc}")
         async with aiohttp.ClientSession() as session:
             try:
-                async with session.get(
-                    replace_vars(url, Root.scope),
-                    allow_redirects=allow_redirects,
-                    **kwargs
-                ) as request:
-                    if mode == "status":  # pyright: ignore [reportUnnecessaryComparison]
-                        await send(
-                            ctx,
-                            discord.Embed(
-                                title=f"Status {(status := str(request.status))}",
-                                description=f"{(f'{t}.' if (t := responses.get(status[0])) else '')}"
-                                            f"{f' ‒ {r}.' if (r := request.reason) else ''}".strip(),  # type: ignore
-                                color=discord.Color.blurple(),
-                                url=f"https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/{status}"
-                            )
-                        )
-                    elif mode == "json":  # pyright: ignore [reportUnnecessaryComparison]
-                        try:
-                            json_dict = await request.json()
-                            with io.BytesIO() as binary_file:
-                                binary_file.write(json.dumps(json_dict, indent=2).encode("utf-8"))
-                                binary_file.seek(0)
-                                await send(ctx, discord.File(filename="response.json", fp=binary_file))
-                        except json.JSONDecodeError:
-                            await send(ctx, "Unable to decode to JSON.")
-                    elif mode == "text":  # pyright: ignore [reportUnnecessaryComparison]
-                        file = await request.text()
-                        await send(ctx, discord.File(filename="response.txt", fp=io.BytesIO(file.encode("utf-8"))))
-                    elif mode == "read":  # pyright: ignore [reportUnnecessaryComparison]
-                        file = await request.read()
-                        with io.BytesIO() as binary_file:
-                            binary_file.write(file)
-                            binary_file.seek(0)
-                            await send(ctx, discord.File(filename="response", fp=binary_file))
+                request = await session.get(replace_vars(url, Root.scope), allow_redirects=allow_redirects, **kwargs)
             except aiohttp.InvalidURL:
-                await send(ctx, "Invalid URL link.")
+                return await send(ctx, "Invalid URL link.")
             except aiohttp.ClientConnectorError:
-                await send(ctx, "Cannot connect to host. Name or service not known.")
+                return await send(ctx, "Cannot connect to host. Name or service not known.")
+            if mode == "status":  # pyright: ignore [reportUnnecessaryComparison]
+                status = str(request.status)
+                status_type = responses[status[0]]
+                colors = {
+                    "1": discord.Color.light_grey(),
+                    "2": discord.Color.green(),
+                    "3": discord.Color.gold(),
+                    "4": discord.Color.red(),
+                    "5": discord.Color.dark_red()
+                }
+                await send(
+                    ctx,
+                    discord.Embed(
+                        title=f"Status {status}",
+                        description=f"{status_type} ‒ {request.reason}".strip(),  # type: ignore
+                        color=colors[status[0]],
+                        url=f"https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/{status}"
+                    )
+                )
+            elif mode == "json":  # pyright: ignore [reportUnnecessaryComparison]
+                try:
+                    js = await request.json()
+                except (json.JSONDecodeError, aiohttp.ContentTypeError):
+                    return await send(ctx, "Unable to decode to JSON.")
+                with io.BytesIO() as binary_file:
+                    binary_file.write(json.dumps(js, indent=2).encode("utf-8"))
+                    binary_file.seek(0)
+                    await send(ctx, discord.File(filename="response.json", fp=binary_file))
+            elif mode == "read":  # pyright: ignore [reportUnnecessaryComparison]
+                data = await request.read()
+                if not data:
+                    return await send(ctx, "Response was empty.")
+                file_ext = CONTENT_TYPES.get(request.content_type, "txt")
+                with io.BytesIO() as binary_file:
+                    binary_file.write(data)
+                    binary_file.seek(0)
+                    await send(ctx, discord.File(filename=f"response.{file_ext}", fp=binary_file))
