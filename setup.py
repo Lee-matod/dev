ff8a19ad448f004126b985ef318a160536ff5c22
Lee-matod <lee.underscore.matod@gmail.com>
2023-01-20 21:10:33 -0300

    Add package data

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setup(
     url="https://github.com/Lee-matod/dev",
     version=version,
     packages=packages,
+    package_data={"dev": ["py.typed"]},
     license="MIT",
     description="A debugging, testing and editing cog for discord.py",
     long_description=readme,
