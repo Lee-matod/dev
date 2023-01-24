feedadff7e1482b77a97bdfc357dfc69da2a214f
Lee-matod <lee.underscore.matod@gmail.com>
2023-01-24 15:36:42 -0300

    Use pathlib for paths

--- dev/config/management.py
+++ dev/config/management.py
@@ -12,7 +12,6 @@ Directory and path-related commands.
 from __future__ import annotations
 
 import io
-import os
 import pathlib
 import shutil
 from typing import TYPE_CHECKING
@@ -38,7 +37,7 @@ class RootManagement(Root):
 
     def __init__(self, bot: types.Bot) -> None:
         super().__init__(bot)
-        self.cwd: str = os.getcwd() + "/"
+        self.cwd: pathlib.Path = pathlib.Path().absolute()
         self.explorer_rgs: list[ManagementRegistration] = []
 
     @root.command(name="cwd", parent="dev", root_placeholder=True, aliases=["change_cwd"])
@@ -47,10 +46,11 @@ class RootManagement(Root):
         if new_cwd is None:
             return await send(ctx, f"Current working directory is: `{self.cwd}`")
         new_cwd = new_cwd.replace("|root|", Settings.root_folder)
-        if not pathlib.Path(new_cwd).exists():
+        path = pathlib.Path(new_cwd)
+        if not path.exists():
             return await send(ctx, f"Directory `{new_cwd}` does not exist.")
-        self.cwd = new_cwd + "/" if not new_cwd.endswith("/") else new_cwd
-        await ctx.message.add_reaction("☑")
+        self.cwd = path
+        await ctx.message.add_reaction("\u2611")
 
     @root.group(name="explorer", parent="dev", ignore_extra=True, invoke_without_command=True)
     async def root_explorer(self, ctx: commands.Context[types.Bot]):
@@ -91,28 +91,29 @@ class RootManagement(Root):
             files_exist: list[str] = []
             for attachment in attachments:
                 data = await attachment.read()
+                as_path = pathlib.Path(attachment.filename)
                 try:
-                    with open(directory + attachment.filename, "xb") as fp:
+                    with open(f"{(directory / as_path).absolute()}", "xb") as fp:
                         fp.write(data)
                 except FileExistsError:
                     files_exist.append(attachment.filename)
                 else:
                     self.explorer_rgs.append(
-                        ManagementRegistration(directory + attachment.filename, ManagementOperation.UPLOAD)
+                        ManagementRegistration(f"{(directory / as_path).absolute()}", ManagementOperation.UPLOAD)
                     )
             if files_exist:
                 await send(
                     ctx,
                     f"{plural(len(files_exist), 'File', False)} " f"{', '.join(files_exist)} already exist.",
                 )
-            return await ctx.message.add_reaction("☑")
+            return await ctx.message.add_reaction("\u2611")
         path = pathlib.Path(directory)
         if path.exists() and ctx.invoked_with == "upload":
             await self.root_explorer_show(ctx, directory=folder)
         elif not path.exists() and not path.suffix:  # create new directory
             path.mkdir()
             self.explorer_rgs.append(ManagementRegistration(f"{path.absolute()}", ManagementOperation.CREATE))
-            await ctx.message.add_reaction("☑")
+            await ctx.message.add_reaction("\u2611")
         elif path.suffix:  # create new empty file
             try:
                 fh = path.open("x", encoding="utf-8")
@@ -120,7 +121,7 @@ class RootManagement(Root):
             except (FileExistsError, UnicodeError):
                 return await send(ctx, f"File {str(path.absolute())} already exists.")
             self.explorer_rgs.append(ManagementRegistration(f"{path.absolute()}", ManagementOperation.CREATE))
-            await ctx.message.add_reaction("☑")
+            await ctx.message.add_reaction("\u2611")
         elif path.exists():
             await send(ctx, f"Path {str(path.absolute())} already exists.")
 
@@ -142,14 +143,13 @@ class RootManagement(Root):
         This command does not change the file's name. Consider using `dev explorer rename`.
         Use `!` at the beginning of the directory to ignore the current working directory.
         """
-        directory = self.format_path(directory, tailing=False)
-        path = pathlib.Path(directory)
+        path = self.format_path(directory)
         if not path.is_file():
             return await send(ctx, f"File `{directory}` does not exist.")
         with path.open("wb") as file:
             file.write(await attachment.read())
         self.explorer_rgs.append(ManagementRegistration(f"{path.absolute()}", ManagementOperation.EDIT))
-        await ctx.message.add_reaction("☑")
+        await ctx.message.add_reaction("\u2611")
 
     @root.command(name="rename", parent="dev explorer", root_placeholder=True)
     async def root_explorer_rename(self, ctx: commands.Context[types.Bot], old_name: str, new_name: str):
@@ -160,15 +160,13 @@ class RootManagement(Root):
         directory.
         The new item's name can't already exist.
         """
-        old_name = self.format_path(old_name)
-        old_path = pathlib.Path(old_name)
-        new_name = new_name.lstrip("/")
+        old_path = self.format_path(old_name)
         if new_name.startswith("?"):
             new_path = pathlib.Path(self.format_path(new_name.lstrip("?")))
         elif new_name.startswith("!"):
             new_path = pathlib.Path(self.format_path(new_name))
         else:
-            new_path = pathlib.Path(str(old_path.parent.absolute()) + "/" + self.format_path("!" + new_name))
+            new_path = pathlib.Path(old_path.parent.absolute() / self.format_path("!" + new_name))
         if not old_path.exists():
             return await send(ctx, f"Path `{str(old_path.absolute())}` does not exist.")
         if new_path.exists():
@@ -178,7 +176,7 @@ class RootManagement(Root):
         except FileNotFoundError:
             return await send(ctx, "Could not locate parent directory or it simply does not exist.")
         self.explorer_rgs.append(ManagementRegistration(f"{old_path}", ManagementOperation.RENAME, f"{new_path}"))
-        await ctx.message.add_reaction("☑")
+        await ctx.message.add_reaction("\u2611")
 
     @root.command(
         name="show",
@@ -193,17 +191,16 @@ class RootManagement(Root):
         If `directory` is None, then it will show the tree of the current working directory.
         Use `!` at the beginning of the directory to ignore the current working directory.
         """
-        directory = self.format_path(directory, tailing=False) or self.cwd
-        path = pathlib.Path(directory)
         assert ctx.invoked_with is not None
+        path = self.format_path(directory) or self.cwd
         if not path.exists():
             return await send(ctx, f"Path `{str(path.absolute())}` does not exist.")
         if ctx.invoked_with.startswith("tree"):
             if not path.is_dir():
                 return await send(ctx, "Path is not a directory.")
             tree = [
-                f"📂 {escape(directory) or '/'}",
-                *[f"\t ├─ {'📄' if sub.is_file() else '📁'} " + escape(f"{sub.name}") for sub in path.iterdir()],
+                f"\U0001f4c2 {escape(directory) or '/'}",
+                *[f"\t ├─ {'\U0001f4c4' if sub.is_file() else '\U0001f4c1'} " + escape(f"{sub.name}") for sub in path.iterdir()],
             ]
             if len(tree) == 1:
                 return await send(ctx, "Directory is empty.")
@@ -229,19 +226,18 @@ class RootManagement(Root):
         Use `!` at the beginning of the directory to ignore the current working directory.
         A prompt will be shown if a directory is specified, and it is not empty.
         """
-        directory = self.format_path(directory)
-        path = pathlib.Path(directory)
+        path = self.format_path(directory)
         if not path.exists():
             return await send(ctx, f"Path `{str(path.absolute())}` does not exist.")
         if path.is_file():
             path.unlink()
-            return await ctx.message.add_reaction("☑")
+            return await ctx.message.add_reaction("\u2611")
         if any(path.iterdir()):
 
             async def func() -> None:
                 shutil.rmtree(path.name)  # thank you aperture!
                 self.explorer_rgs.append(ManagementRegistration(f"{path.absolute()}", ManagementOperation.DELETE))
-                await ctx.message.add_reaction("☑")
+                await ctx.message.add_reaction("\u2611")
 
             await send(
                 ctx,
@@ -252,17 +248,13 @@ class RootManagement(Root):
         else:
             path.rmdir()
             self.explorer_rgs.append(ManagementRegistration(f"{path.absolute()}", ManagementOperation.DELETE))
-            await ctx.message.add_reaction("☑")
+            await ctx.message.add_reaction("\u2611")
 
-    def format_path(self, directory: str, *, tailing: bool = True) -> str:
+    def format_path(self, directory: str) -> pathlib.Path:
         """Format the given filepath to the correct directory."""
-        directory = directory.replace("\\", "/").replace("|root|", Settings.root_folder)
+        directory = directory.replace("|root|", Settings.root_folder)
         if not directory.startswith("!"):
-            directory = self.cwd + directory.lstrip("/")
-        if tailing and not directory.endswith("/"):
-            directory += "/"
-        elif not tailing and directory.endswith("/"):
-            directory = directory[:-1]
-        if directory.startswith("!"):
-            directory = "/" + directory.replace("!", "", 1).lstrip("/")
-        return directory
+            path = self.cwd / pathlib.Path(directory)
+        else:
+            path = pathlib.Path(directory.replace("!", "", 1))
+        return path.absolute()
