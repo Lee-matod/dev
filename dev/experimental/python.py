8f0feaea643149c6b6944a489cffa2b7a54060c2
Lee-matod <lee.underscore.matod@gmail.com>
2023-03-07 15:16:50 -0500

    Add black command

--- dev/experimental/python.py
+++ dev/experimental/python.py
@@ -13,6 +13,7 @@ from __future__ import annotations
 
 import asyncio
 import contextlib
+import re
 import sys
 from collections.abc import Sequence
 from typing import TYPE_CHECKING, Any
@@ -21,8 +22,10 @@ import discord
 from discord.ext import commands
 
 from dev import root
+from dev.converters import str_ints
 from dev.handlers import ExceptionHandler, GlobalLocals, RelativeStandard, replace_vars
 from dev.interpreters import Execute
+from dev.types import Annotated
 from dev.utils.functs import send
 from dev.utils.startup import Settings
 from dev.utils.utils import clean_code, codeblock_wrapper
@@ -31,6 +34,24 @@ if TYPE_CHECKING:
     from types import TracebackType
 
     from dev import types
+
+try:
+    import black
+    from black.mode import TargetVersion
+    from black.parsing import InvalidInput
+except ModuleNotFoundError:
+    _HASBLACK: bool = False  # type: ignore
+else:
+    _HASBLACK: bool = True
+
+_comment = r"^#[ ]*black:[ ]*"
+_BLACK_BOOL = re.compile(
+    _comment
+    + r"(enable|disable)[ ]*=[ ]*(string-normalization|pyi|skip-source-first-line|magic-trailing-comma|preview)$",
+    re.MULTILINE,
+)
+_BLACK_LINELENGTH = re.compile(_comment + r"line-length=([0-9]+)$", re.MULTILINE)
+_BLACK_PYVERSION = re.compile(_comment + r"target-versions?=([0-9, ]+)$", re.MULTILINE)
 
 
 def _maybe_raw_send(item: Any, /) -> bool:
@@ -157,6 +178,50 @@ class RootPython(root.Container):
                 self.last_output = None
         finally:
             reader_task.cancel()
+
+    if _HASBLACK:
+
+        @root.command(name="black", parent="dev", require_var_positional=True)
+        async def root_black(self, ctx: commands.Context[types.Bot], *, code: Annotated[str, clean_code]):
+            """Format a piece of code using the uncompromising black formatter.
+
+            Adjust the formatter to your liking by using comments as shown in the
+            example below.
+            ```
+            # black: enable=magic-trailing-comma
+            # black: disable=string-normalization
+            # black: line-length=120
+            # black: target-version=39
+            ```
+            Enabling or disabling multiple settings in a single comment is not supported.
+
+            This feature is only available if the black module is installed and detected.
+            """
+            fm = black.FileMode()
+            pyversion = _BLACK_PYVERSION.search(code)
+            linelength = _BLACK_LINELENGTH.search(code)
+            bool_settings: list[tuple[str, bool]] = [
+                (match[1], True if match[0] == "enable" else False) for match in _BLACK_BOOL.findall(code)
+            ]
+            if pyversion is not None:
+                versions: set[int] = set(str_ints(pyversion.group(1).replace(".", "")))
+                target_versions: set[TargetVersion] = set()
+                for ver in versions:
+                    try:
+                        obj = TargetVersion[f"PY{ver}"]
+                    except ValueError:
+                        continue
+                    target_versions.add(obj)
+                fm.target_versions = target_versions
+            if linelength is not None:
+                fm.line_length = int(linelength.group(1))
+            for setting, toggled in bool_settings:
+                setattr(fm, setting.replace("-", "_"), toggled)
+            try:
+                formatted = black.format_str(code, mode=fm)
+            except InvalidInput as exc:
+                return await send(ctx, f"Syntax error: {exc}")
+            await send(ctx, codeblock_wrapper(formatted, "py"))
 
     async def _on_update(self, ctx: commands.Context[types.Bot], view: list[str], /) -> None:
         current = len(view)
