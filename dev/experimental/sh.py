94bf29a050245ef3962d0ef56119d84378b1ebe9
Lee-matod <lee.underscore.matod@gmail.com>
2023-03-08 13:41:22 -0500

    Add black executable command

--- dev/experimental/sh.py
+++ dev/experimental/sh.py
@@ -12,19 +12,53 @@ Shell interpreter commands.
 from __future__ import annotations
 
 import asyncio
+import pathlib
+import re
+import shutil
+import tempfile
 from typing import TYPE_CHECKING
 
 import discord
 from discord.ext import commands
 
 from dev import root
+from dev.converters import MessageCodeblock, codeblock_converter
 from dev.interpreters import ShellSession
 from dev.types import Annotated
 from dev.utils.functs import send
-from dev.utils.utils import clean_code
+from dev.utils.utils import clean_code, codeblock_wrapper
 
 if TYPE_CHECKING:
     from dev import types
+
+try:
+    import black  # type: ignore
+except ModuleNotFoundError:
+    _HASBLACK = False  # type: ignore
+else:
+    _HASBLACK = True
+
+BLACK_LINE_LENGTH = re.compile(r"(-l|--line-length) ([0-9]+)")
+BLACK_TARGET_VERSION = re.compile(
+    r"(-t|--target-version) (" + r"|".join(f"py3{vernum}" for vernum in range(3, 12)) + r")"
+)
+BLACK_BOOLEANS = (
+    "--pyi",
+    "--skip-source-first-line",
+    "-x",
+    "--skip-string-normalization",
+    "-S",
+    "--skip-magic-trailing-comma",
+    "--preview",
+    "--check",
+    "--diff",
+    "--color",
+    "--fast",
+    "--quiet",
+    "-q",
+    "--verbose",
+    "-v",
+)
 
 
 class RootShell(root.Container):
@@ -32,7 +66,7 @@ class RootShell(root.Container):
 
     def __init__(self, bot: types.Bot) -> None:
         super().__init__(bot)
-        self.active_shell_sessions: list[int] = []
+        self.active_shell_sessions: dict[int, ShellSession] = {}
 
     @root.command(name="shell", parent="dev", aliases=["sh", "cmd", "bash", "ps"])
     async def root_shell(self, ctx: commands.Context[types.Bot], *, script: Annotated[str, clean_code]):
@@ -42,12 +76,10 @@ class RootShell(root.Container):
         Type `exit` to quit the session.
         """
         if ctx.author.id in self.active_shell_sessions:
-            return await send(
-                ctx,
-                "A shell session is already active, please close it before starting a new one.",
-            )
-        self.active_shell_sessions.append(ctx.author.id)
+            session = self.active_shell_sessions[ctx.author.id]
+            session.terminated = True
         shell = ShellSession()
+        self.active_shell_sessions[ctx.author.id] = shell
         try:
             with shell(script) as process:
                 await process.run_until_complete(ctx)
@@ -73,6 +105,7 @@ class RootShell(root.Container):
                     )
                 with shell(message.content[len(shell.interface) :].strip()) as process:
                     await process.run_until_complete(ctx)
+                await asyncio.sleep(0)
             await send(
                 ctx,
                 shell.set_exit_message(f"Return code: `{process.close_code}`"),
@@ -80,4 +113,54 @@ class RootShell(root.Container):
                 paginator=shell.paginator,
             )
         finally:
-            self.active_shell_sessions.remove(ctx.author.id)
+            try:
+                del self.active_shell_sessions[ctx.author.id]
+            except KeyError:
+                pass
+
+    if shutil.which("black") and not _HASBLACK:
+
+        @root.command(name="black", parent="dev", require_var_positional=True)
+        async def root_black(
+            self, ctx: commands.Context[types.Bot], *, code: Annotated[MessageCodeblock, codeblock_converter]
+        ):
+            """Format a piece of code using the uncompromising black formatter.
+
+            Invokes the system shell. Arguments that are before the codeblock will be
+            forwarded to black's executable options.
+
+            This feature is only available if the black executable is detected.
+            """
+            cmd_args = code.content
+            script = code.codeblock
+            if script is None:
+                return await send(ctx, "Malformed arguments were given.")
+
+            enabled_options: list[str] = [opt for opt in BLACK_BOOLEANS if opt in cmd_args.split()]
+
+            for compiled in (BLACK_LINE_LENGTH, BLACK_TARGET_VERSION):
+                match = compiled.search(cmd_args)
+                if match:
+                    enabled_options.append(f"{match.group(1)} {match.group(2)}")
+
+            with tempfile.TemporaryDirectory() as directory:
+                path = pathlib.Path(directory)
+                main_tmp = path / "main.py"
+                main_tmp.touch()
+                with main_tmp.open("w") as fp:
+                    fp.write(script)
+                full = f"cd {directory} && black main.py"
+                if enabled_options:
+                    full = f"cd {directory} && black {' '.join(enabled_options)} main.py"
+
+                session = ShellSession()
+                with session(full) as proc:
+                    output = await proc.run_until_complete()
+                    if output is not None:
+                        await send(ctx, codeblock_wrapper(output, session.highlight), forced=True)
+
+                with main_tmp.open() as fp:
+                    new_file = fp.read()
+                    if new_file == script:
+                        return
+                await send(ctx, codeblock_wrapper(new_file, "py"), forced=True)
